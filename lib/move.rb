58002537c7c865100671bdc968a02c380b1ba7ea
Alice Brunel <alc.brunel@gmail.com>
2019-06-28 17:18:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,9 +10,9 @@ def move(array, index, value="X")
 end
 
 def display_board(array)
-  puts " #{array[0]} | #{board[1]} | #{board[2]} "
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
